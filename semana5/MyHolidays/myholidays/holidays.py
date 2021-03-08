3658c407f91694b4102d1f8b480a521c11076e01
lucasgomes-napp <lucas.gomes@nappsolutions.com>
2021-03-07 22:22:09 -0300

    Passaram 20 testes

--- semana5/MyHolidays/myholidays/holidays.py
+++ semana5/MyHolidays/myholidays/holidays.py
@@ -1,45 +1,46 @@
-from datetime import date, datetime
+import datetime
 
 class MyCalendar:
-	
-   # Converte string para datas se for válido, se não for válido, zera o campo
-   def convertdate(self, args):
-      if type(args) == str:
-         try:
-               args = datetime.datetime.strptime(args, '%d/%m/%Y').date()
-         except ValueError:
-               args = 0
-      return args
 
-   def __init__(self, *args):
-      self.datas = []
-      self.temp = []
+    # Converte string para datas se for válido, se não for válido, zera o campo
+    def convertdate(self, args):
+        if type(args) == str:
+            try:
+                args = datetime.datetime.strptime(args, '%d/%m/%Y').date()
+            except ValueError:
+                args = 0
+        return args
 
-      # Adiciona a data em uma lista self.temporária
-      for dt in args:
-         self.temp.append(self.convertdate(dt))
+    def __init__(self, *args):
+        self.datas = []
+        self.temp = []
 
-      # Lê as datas da lista temp e adiciona somente os valores válidos
-      for dt in self.temp:
-         if type(dt) != int and type(dt) != list and type(dt) != tuple:
-               self.datas.append(self.convertdate(dt))
-   def add_holiday(self, *args):
+        # Adiciona a data em uma lista self.temporária
+        for dt in args:
+            self.temp.append(self.convertdate(dt))
 
-      # Adiciona a data em uma lista self.temporária sem duplicar
-      for dt in args:
-         if self.convertdate(dt) not in self.temp:
-               self.temp.append(self.convertdate(dt))
+        # Lê as datas da lista temp e adiciona somente os valores válidos
+        for dt in self.temp:
+            if type(dt) != int and type(dt) != list and type(dt) != tuple:
+                self.datas.append(self.convertdate(dt))
 
-      # Lê as datas da lista temp e adiciona somente os valores válidos
-      for dt in self.temp:
-         if self.convertdate(dt) not in self.datas and type(dt) != int:
-               self.datas.append(self.convertdate(dt))
-   
-   def check_holiday(self, args):
-      if self.convertdate(args) in self.datas:
-         print(args)
-         isholiday = True
-      else:
-         isholiday = False
+    def add_holiday(self, *args):
 
-      return isholiday
+        # Adiciona a data em uma lista self.temporária sem duplicar
+        for dt in args:
+            if self.convertdate(dt) not in self.temp:
+                self.temp.append(self.convertdate(dt))
+
+        # Lê as datas da lista temp e adiciona somente os valores válidos
+        for dt in self.temp:
+            if self.convertdate(dt) not in self.datas and type(dt) != int:
+                self.datas.append(self.convertdate(dt))
+
+    def check_holiday(self, args):
+        if self.convertdate(args) in self.datas:
+            print(args)
+            isholiday = True
+        else:
+            isholiday = False
+
+        return isholiday
